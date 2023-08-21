578875c6508499d1ba918666a14ef69ed464775c
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-08-21 07:26:14 -0700

    CHAR,STRING & PERCENTAGE

--- main.h
+++ main.h
@@ -9,6 +9,20 @@
 #define BUFF_SIZE 1024
 #define UNUSED(x) (void)(x)
 
+/* FLAGS */
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+
+#define S_LONG 2
+#define S_SHORT 1
+
+
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
